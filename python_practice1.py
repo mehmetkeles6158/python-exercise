7671a0e53dd470354322218ddd392c7138b909ca
Mehmet <mehmetk6158@gmail.com>
2022-01-31 12:34:51 -0500

    try and except

--- python_practice1.py
+++ python_practice1.py
@@ -1,1 +1,15 @@
-print("hello world")
+# print("hello world")
+# x = 0
+# y = 0
+# if x == 0:
+#     if y == 10:
+#         print("Yes")
+
+str = 'black eagles'
+
+try:
+    astr = int(str)
+except:
+    astr = -1
+
+print(astr)
