62c3feb13ea612ae471ec4477176ed1e3f6e8481
Mehmet <mehmetk6158@gmail.com>
2022-01-31 14:50:05 -0500

    smallest number

--- python_practice1.py
+++ python_practice1.py
@@ -14,5 +14,13 @@
 
 # print(astr)
 
-for i in [2, 1, 5]:
-    print(i)
+# for i in [2, 1, 5]:
+#     print(i)
+
+smallest = None
+print("Before:", smallest)
+for itervar in [3, 41, 12, 9, 74, 15]:
+    if smallest is None or itervar < smallest:
+        smallest = itervar
+    print("Loop:", itervar, smallest)
+print("Smallest:", smallest)
