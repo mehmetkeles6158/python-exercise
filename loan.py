9918b446d9ca79c4881c7adabfd1433afc4dca5d
Mehmet <mehmetk6158@gmail.com>
2022-02-05 23:47:55 -0500

    loan cal step-2

--- loan.py
+++ loan.py
@@ -1,3 +1,6 @@
 # Get the loan details
 
 money_owed = float(input("How much money do you owe, in dollars?\n"))  # $50000
+apr = float(input("What is annual rate?\n"))  # 3%
+payment = float(
+    input("What will be your montly payment be, in dollars?\n"))  # $50000
