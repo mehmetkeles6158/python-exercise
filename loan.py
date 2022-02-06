5fc9831e9d5740e366f68ea903368abbaba13a08
Mehmet <mehmetk6158@gmail.com>
2022-02-05 23:52:58 -0500

    loan cal step-3

--- loan.py
+++ loan.py
@@ -3,4 +3,9 @@
 money_owed = float(input("How much money do you owe, in dollars?\n"))  # $50000
 apr = float(input("What is annual rate?\n"))  # 3%
 payment = float(
-    input("What will be your montly payment be, in dollars?\n"))  # $50000
+    input("What will be your montly payment be, in dollars?\n"))  # 1000
+months = int(input("How many months do you want to see results for?\n"))  # 24
+
+# Divide apr by 100 to make it a percent, then divide by 12 to make monthly
+
+monthly_rate = apr / 100 / 12
