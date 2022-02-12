7a8d96f922e3366b14f87a611ff2a040d8cf027c
Mehmet <mehmetk6158@gmail.com>
2022-02-12 15:38:10 -0500

    loop_examples-1

---
+++ loop_exapmles.py
@@ -0,0 +1,18 @@
+# letters = []
+
+# for x in "apple":
+#     letters.append(x)
+
+# print(letters)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     if x == "banana":
+#         break
+#     print(x)
